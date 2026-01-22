c43bfbc7728ec4a87ffab907b8b553792556c0b9
zoll-h1 <nurbekerkibaev9@gmail.com>
2026-01-22 22:04:40 +0600

    chore: setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import Intro from "./sections/Intro/Intro";
-import About from "./sections/About/About";
-import TableOfContent from "./sections/TableOfContent/TableOfContent";
-import Projects from "./sections/Projects/Projects"
-import Thanks from "./sections/Thanks/Thanks";
-
 export default function App() {
     return (
-        <div>
-            <Intro />
-            <About />
-            <TableOfContent />
-            <Projects />
-            <Thanks />
+        <div classname="min-h-screen bg-black text-white flex items-center justify-center">
+            <h1 classname="text-5xl font-bold">Tailwind OK</h1>
         </div>
     );
 }
